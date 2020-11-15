37380e568c6d7aab20dc0678a889e6bfc0ef3481
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-11-15 11:02:26 +0500

    reminder

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import List from "./List";
+import data from "./data";
+import { reminder } from "./types/type";
 function App() {
+  const [people, setPeople] = React.useState<reminder[]>(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
---
+++ src/types/type.ts
@@ -0,0 +1,6 @@
+export type reminder = {
+  id: number;
+  name: string;
+  age: number;
+  image: string;
+};
